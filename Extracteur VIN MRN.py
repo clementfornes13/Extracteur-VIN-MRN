695c9bace65214f568b8e7b15a879b7ca4eaa8c1
clementfornes13 <clement.fornes@viacesi.fr>
2023-06-30 15:15:00 +0200

    Final version + New GUI + choice of extract

--- Extracteur VIN MRN.py
+++ Extracteur VIN MRN.py
@@ -1,163 +1,222 @@
-from tkinter import *
-from tkinter import filedialog, messagebox
-from tkinter.ttk import Progressbar
+##############################################################################
+# Nom du fichier: Extracteur VIN MRN.py
+# Description: Ce script permet d'extraire les VIN et MRN de fichiers PDF
+#
+# Auteur: FORNES Clément
+# Date: 30 juin 2023
+#
+# Copyright (c) 2023 FORNES Clément
+#
+# Licence: MIT License
+##############################################################################
+# MIT License
+#
+# Copyright (c) 2023 FORNES Clément
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ------------------------------------------------------------------------------
+#
+# Ce script permet d'extraire les VIN et MRN de fichiers PDF
+# Il est possible d'extraire les VIN, les MRN ou les deux
+# Les VIN et MRN sont enregistrés dans un fichier Excel
+# Le fichier Excel est enregistré dans le dossier de destination
+# Mise à jour: 30 juin 2023
+#################### IMPORTS ####################
+from tkinter import Tk, Label, Button, Entry, filedialog, messagebox, PhotoImage, IntVar, END
+from tkinter.ttk import Progressbar, Frame, Radiobutton
 from openpyxl import Workbook
 from PyPDF2 import PdfReader
-import os
-import re
+from os import path, startfile, listdir
+from re import findall
 import threading
+#################################################
 
+#################### CLASSES ####################
 class VINMRNExtractor(Tk):
+
     def __init__(self):
+        # Initialisation de la fenêtre principale
         super().__init__()
-        self.ListeVIN = []
-        self.ListeMRN = []
-        self.unique_VIN = []
-        self.unique_MRN = []
-        self.IMG_PATH = os.path.join(os.path.dirname(__file__), 'images')
+        self.ListeVIN, self.ListeMRN, self.unique_VIN, self.unique_MRN = [], [], [], []
+        self.IMG_PATH = path.join(path.dirname(__file__), 'images')
         self.VIN_PATTERN = r"(?!FR)[A-HJ-NPR-Z][A-HJ-NPR-Z0-9]{15}[A-HJ-NPR-Z\d]"
         self.MRN_PATTERN = r"(?<!\S)(?!MRN)(?=[A-Z0-9]{18}\b)[A-Z0-9]*[A-Z][A-Z0-9]*\b(?!\S)"
-        self.icon = os.path.join(self.IMG_PATH, 'Icone.ico').replace('\\', '/')
-        self.logo = os.path.join(self.IMG_PATH, 'Logo TEA FOS.png').replace('\\', '/')
+        self.icon = path.join(self.IMG_PATH, 'Icone.ico').replace('\\', '/')
+        self.logo = path.join(self.IMG_PATH, 'Logo TEA FOS.png').replace('\\', '/')
         self.initUI()
 
     def initUI(self):
-        self.title("Extracteur VIN MRN PDF EAD")
+        # Initialisation de l'interface graphique
+        # Paramètres de la fenêtre
+        self.title("Extracteur VIN MRN")
         self.iconbitmap(self.icon)
-        self.geometry("450x200")
-
+        self.geometry("350x350")
         logoLabel = Label(self)
         logoImage = PhotoImage(file=self.logo)
         logoLabel.config(image=logoImage)
         logoLabel.image = logoImage
         logoLabel.grid(row=0, column=0, pady=10)
-
+        # Frame
         frame = Frame(self)
         frame.grid(row=1, column=0)
-
+        # Dossier PDF
         labelPDF = Label(frame, text="Dossier PDF:")
         labelPDF.grid(row=0, column=0, padx=5, pady=5)
         self.inputPDF = Entry(frame, width=30)
         self.inputPDF.grid(row=0, column=1, padx=5, pady=5)
         browsePDF = Button(frame, text="Parcourir", command=self.browsePDFClicked)
         browsePDF.grid(row=0, column=2, padx=5, pady=5)
-
+        # Destination
         labelDest = Label(frame, text="Destination:")
         labelDest.grid(row=1, column=0, padx=5, pady=5)
         self.inputDest = Entry(frame, width=30)
         self.inputDest.grid(row=1, column=1, padx=5, pady=5)
         browseDest = Button(frame, text="Parcourir", command=self.browseDestClicked)
         browseDest.grid(row=1, column=2, padx=5, pady=5)
-
-        extractButton = Button(self, text="Extraction", command=self.launch_extraction)
-        extractButton.grid(row=2, column=0, pady=10)
-
-        self.progress_bar = Progressbar(self, mode='determinate')
-        self.progress_bar.grid(row=3, column=0, pady=5)
-        self.progress_bar.grid_remove()
-
+        # Boutons
+        self.extractButton = Button(self, text="Extraction", command=self.launch_extraction)
+        self.extractButton.grid(row=5, column=0, pady=10)
+        # Options d'extraction
         self.extractOption = IntVar()
+        # 1 = Extraction VIN
+        self.vinRadio = Radiobutton(self, text="VIN", variable=self.extractOption, value=1)
+        self.vinRadio.grid(row=2, column=0, padx=5, pady=5, sticky='n')
+        # 2 = Extraction MRN
+        self.mrnRadio = Radiobutton(self, text="MRN", variable=self.extractOption, value=2)
+        self.mrnRadio.grid(row=3, column=0, padx=5, pady=5, sticky='n')
+        # 3 = Extraction VIN et MRN
+        self.vinMrnRadio = Radiobutton(self, text="VIN + MRN", variable=self.extractOption, value=3)
+        self.vinMrnRadio.grid(row=4, column=0, padx=5, pady=5, sticky='n')
+        # Barre de progression
+        self.progress_bar = Progressbar(self, mode='determinate')
+        self.progress_bar.grid(row=6, column=0, pady=5)
+        self.progress_bar.grid_remove()
+    def disable_buttons(self):
+        self.extractButton.config(state="disabled")
+        self.vinRadio.config(state="disabled")
+        self.mrnRadio.config(state="disabled")
+        self.vinMrnRadio.config(state="disabled")
 
-        vinRadio = Radiobutton(self, text="Extraction VIN", variable=self.extractOption, value=1)
-        vinRadio.grid(row=4, column=0, padx=5, pady=5)
+    def enable_buttons(self):
+        self.extractButton.config(state="normal")
+        self.vinRadio.config(state="normal")
+        self.mrnRadio.config(state="normal")
+        self.vinMrnRadio.config(state="normal")
 
-        mrnRadio = Radiobutton(self, text="Extraction MRN", variable=self.extractOption, value=2)
-        mrnRadio.grid(row=5, column=0, padx=5, pady=5)
-
-        vinMrnRadio = Radiobutton(self, text="Extraction VIN et MRN", variable=self.extractOption, value=3)
-        vinMrnRadio.grid(row=6, column=0, padx=5, pady=5)
-        
     def browsePDFClicked(self):
+        # Selection du dossier contenant les PDF
         if directory := filedialog.askdirectory(title="Choisir un dossier"):
             self.inputPDF.delete(0, END)
             self.inputPDF.insert(0, directory)
 
     def browseDestClicked(self):
+        # Selection du dossier de destination
         if directory := filedialog.askdirectory(title="Choisir un dossier"):
             self.inputDest.delete(0, END)
             self.inputDest.insert(0, directory)
-    def extract_vins(self, emplacement_pdf, destination=None):
+            
+    def extract_vins(self, emplacement_pdf, destination):
+        # Extraction des VIN
         if not emplacement_pdf:
             return None
         workbook = Workbook()
         sheet = workbook.active
         sheet['A1'] = 'Liste des VIN'
         row_num = 2
-        file_list = [pdf for pdf in os.listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
+        file_list = [pdf for pdf in listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
         total_files = len(file_list)
         self.progress_bar['maximum'] = total_files
         self.progress_bar.grid()
         for file_count, pdf in enumerate(file_list, start=1):
-            pdf = os.path.join(emplacement_pdf, pdf).replace("\\", "/")
+            pdf = path.join(emplacement_pdf, pdf).replace("\\", "/")
             with open(pdf, 'rb') as file:
                 lire_pdf = PdfReader(file)
                 for page_num, page in enumerate(lire_pdf.pages):
                     texte = page.extract_text()
-                    vins = re.findall(self.VIN_PATTERN, texte)
+                    vins = findall(self.VIN_PATTERN, texte)
                     self.ListeVIN.extend(vins)
                     for vin_unique in set(self.ListeVIN):
                         self.unique_VIN.append(vin_unique)
                     for vin in self.unique_VIN:
                         sheet.cell(row=row_num, column=1, value=vin)
                         row_num += 1
-                    self.ListeVIN.clear()
-                    self.unique_VIN.clear()
+                    self.ListeVIN.clear(), self.unique_VIN.clear()
             file_count += 1
             self.progress_bar['value'] = file_count
             self.update()
         if not destination:
+            self.enable_buttons()
             return None
         file_name = "Extraction VIN EAD.xlsx".format(1)
-        file_path = os.path.join(destination, file_name)
+        file_path = path.join(destination, file_name)
         i = 1
-        while os.path.exists(file_path):
+        while path.exists(file_path):
             i += 1
             file_name = f"Extraction VIN EAD {i}.xlsx"
-            file_path = os.path.join(destination, file_name)
+            file_path = path.join(destination, file_name)
         workbook.save(file_path)
         return file_path
-    def extract_mrns(self, emplacement_pdf, destination=None):
+    
+    def extract_mrns(self, emplacement_pdf, destination):
+        # Extraction des MRN
         if not emplacement_pdf:
             return None
         workbook = Workbook()
         sheet = workbook.active
         sheet['A1'] = 'Liste des MRN'
-
         row_num = 2
-        file_list = [pdf for pdf in os.listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
+        file_list = [pdf for pdf in listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
         total_files = len(file_list)
         self.progress_bar['maximum'] = total_files
         self.progress_bar.grid()
         for file_count, pdf in enumerate(file_list, start=1):
-            pdf = os.path.join(emplacement_pdf, pdf).replace("\\", "/")
+            pdf = path.join(emplacement_pdf, pdf).replace("\\", "/")
             with open(pdf, 'rb') as file:
                 lire_pdf = PdfReader(file)
                 for page_num, page in enumerate(lire_pdf.pages):
                     texte = page.extract_text()
-                    mrns = re.findall(self.MRN_PATTERN, texte)
+                    mrns = findall(self.MRN_PATTERN, texte)
                     self.ListeMRN.extend(mrns)
                     for mrn_unique in set(self.ListeMRN):
                         self.unique_MRN.append(mrn_unique)
                     for mrn in self.unique_MRN:
                         sheet.cell(row=row_num, column=1, value=mrn)
                         row_num += 1
-                    self.ListeMRN.clear()
-                    self.unique_MRN.clear()
+                    self.ListeMRN.clear(), self.unique_MRN.clear()
             file_count += 1
             self.progress_bar['value'] = file_count
             self.update()
         if not destination:
             return None
         file_name = "Extraction MRN EAD.xlsx".format(1)
-        file_path = os.path.join(destination, file_name)
+        file_path = path.join(destination, file_name)
         i = 1
-        while os.path.exists(file_path):
+        while path.exists(file_path):
             i += 1
             file_name = f"Extraction MRN EAD {i}.xlsx"
-            file_path = os.path.join(destination, file_name)
+            file_path = path.join(destination, file_name)
         workbook.save(file_path)
         return file_path
-    def extract_vins_mrns(self, emplacement_pdf, destination=None):
+
+    def extract_vins_mrns(self, emplacement_pdf, destination):
+        # Extraction des VIN et MRN
         if not emplacement_pdf:
             return None
         workbook = Workbook()
@@ -165,18 +224,18 @@ class VINMRNExtractor(Tk):
         sheet['A1'] = 'Liste des VIN'
         sheet['B1'] = 'Liste des MRN'
         row_num = 2
-        file_list = [pdf for pdf in os.listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
+        file_list = [pdf for pdf in listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
         total_files = len(file_list)
         self.progress_bar['maximum'] = total_files
         self.progress_bar.grid()
         for file_count, pdf in enumerate(file_list, start=1):
-            pdf = os.path.join(emplacement_pdf, pdf).replace("\\", "/")
+            pdf = path.join(emplacement_pdf, pdf).replace("\\", "/")
             with open(pdf, 'rb') as file:
                 lire_pdf = PdfReader(file)
                 for page_num, page in enumerate(lire_pdf.pages):
                     texte = page.extract_text()
-                    vins = re.findall(self.VIN_PATTERN, texte)
-                    mrns = re.findall(self.MRN_PATTERN, texte)
+                    vins = findall(self.VIN_PATTERN, texte)
+                    mrns = findall(self.MRN_PATTERN, texte)
                     self.ListeVIN.extend(vins)
                     self.ListeMRN.extend(mrns)
                     for vin_unique in set(self.ListeVIN):
@@ -185,44 +244,49 @@ class VINMRNExtractor(Tk):
                         sheet.cell(row=row_num, column=1, value=vin)
                         sheet.cell(row=row_num, column=2, value=self.ListeMRN[0])
                         row_num += 1
-                    self.ListeVIN.clear()
-                    self.ListeMRN.clear()
-                    self.unique_VIN.clear()
+                    self.ListeVIN.clear(), self.ListeMRN.clear(), self.unique_VIN.clear()
             file_count += 1
             self.progress_bar['value'] = file_count
             self.update()
         if not destination:
             return None
         file_name = "Extraction VIN MRN EAD.xlsx".format(1)
-        file_path = os.path.join(destination, file_name)
+        file_path = path.join(destination, file_name)
         i = 1
-        while os.path.exists(file_path):
+        while path.exists(file_path):
             i += 1
             file_name = f"Extraction VIN MRN EAD {i}.xlsx"
-            file_path = os.path.join(destination, file_name)
+            file_path = path.join(destination, file_name)
         workbook.save(file_path)
         return file_path
     
     def update_progress(self, value, maximum):
+        # Mise à jour de la barre de progression
         self.progress_bar['value'] = value
         self.progress_bar['maximum'] = maximum
         self.update()
         
+    def extraction_temp(self, extraction_func, args, file_path_temp):
+        file_path_temp[0] = extraction_func(*args)
     def launch_extraction(self):
+        # Lancement de l'extraction
+        self.disable_buttons()
+        file_path_temp = [None]
         emplacement_pdf = self.inputPDF.get()
         destination = self.inputDest.get()
-        file_list = [pdf for pdf in os.listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
+        file_list = [pdf for pdf in listdir(emplacement_pdf) if pdf.lower().endswith('.pdf')]
         total_files = len(file_list)
         if total_files == 0:
             messagebox.showerror('Erreur', 'Le dossier PDF sélectionné est vide')
+            self.enable_buttons()
             return None
         extract_option = self.extractOption.get()
         if extract_option == 1:
-            thread = threading.Thread(target=self.extract_vins, args=(emplacement_pdf, destination))
+            thread = threading.Thread(target=self.extraction_temp, args=(self.extract_vins, (emplacement_pdf, destination), file_path_temp))
         elif extract_option == 2:
-            thread = threading.Thread(target=self.extract_mrns, args=(emplacement_pdf, destination))
+            thread = threading.Thread(target=self.extraction_temp, args=(self.extract_mrns, (emplacement_pdf, destination), file_path_temp))
         else:
-            thread = threading.Thread(target=self.extract_vins_mrns, args=(emplacement_pdf, destination))
+            thread = threading.Thread(target=self.extraction_temp, args=(self.extract_vins_mrns, (emplacement_pdf, destination), file_path_temp))
         thread.start()
         self.progress_bar.grid()
         stop_flag = False
@@ -236,14 +300,17 @@ class VINMRNExtractor(Tk):
         for t in threading.enumerate():
             if t != threading.current_thread():
                 t.join()
-        file_path = self.extract_vins(emplacement_pdf, destination) if extract_option == 1 else \
-                self.extract_mrns(emplacement_pdf, destination) if extract_option == 2 else \
-                self.extract_vins_mrns(emplacement_pdf, destination)
+        file_path = file_path_temp[0]
         if not file_path:
             messagebox.showerror('Erreur', 'Pas de Dossier PDF / Destination choisi')
+            self.enable_buttons()
             return None
         messagebox.showinfo('Extraction réussie', f"Fichier enregistré ici : {file_path}")
-        os.startfile(file_path)
-
+        startfile(file_path)
+        self.progress_bar.grid_remove()
+        self.enable_buttons()
+#################################################
 app = VINMRNExtractor()
+app.resizable(False, False)
 app.mainloop()
+#################################################
